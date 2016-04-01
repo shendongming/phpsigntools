c70453ce8e16cda67e63789dbc857516345436c2
shendongming <sdmzhu3@gmail.com>
2016-04-01 21:47:47 +0800

    init test code

---
+++ phpsigntools.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 对某个代码签名,修改后拒绝运行
+ * User: sdm
+ * Date: 16/4/1
+ * Time: 21:06
+ */
+
+
+
+$file = $_SERVER['argv'][1];
+
+if(!is_file($file)){
+    die("error file:$file\n");
+}
+$code = file_get_contents($file);
+$tpl='<?php $__code=file_get_contents(__FILE__);$__code_arr=explode("\n",$__code,2);$__code_head = $__code_arr[0];$__code_head_arr = explode("//",$__code_head);$sign = array_pop($__code_head_arr);if(md5(substr($__code_arr[1],2))!=$sign){echo substr($__code_arr[1],2);echo $sign;};//';
+$__code=file_get_contents($file);$__code_arr=explode("\n",$__code,2);$__code_head = $__code_arr[0];$__code_head_arr = explode('//',$__code_head);$sign = $__code_head_arr[1];
+
+$tpl.=md5($code)."\n?>";
+$tpl.=$code;
+rename($file,$file.'.bak');
+file_put_contents($file,$tpl);
+echo "\nsign $file ok\n";
+echo "bak $file => $file.bak\n";
---
+++ test_run1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sdm
+ * Date: 16/4/1
+ * Time: 21:08
+ */
+
+
+include_once __DIR__.'/test_lib2.php';
+
+var_dump(test_fun("123"));
