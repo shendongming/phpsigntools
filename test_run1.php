29fd65198d32c15b597de32efbdf1f118258e11b
shendongming <sdmzhu3@gmail.com>
2016-04-01 22:01:49 +0800

    去掉调试信息,demo 完成可以保证代码不容易被修改

--- test_run1.php
+++ test_run1.php
@@ -1,4 +1,4 @@
-<?php $__code=file_get_contents(__FILE__);$__code_arr=explode("\n",$__code,2);$__code_head = $__code_arr[0];$__code_head_arr = explode("//",$__code_head);$sign = array_pop($__code_head_arr);if(md5(substr($__code_arr[1],2))!=$sign){echo substr($__code_arr[1],2);echo $sign;};//48b08bc8b18edd5600f2aaa2b863691e
+<?php $__code=file_get_contents(__FILE__);$__code_arr=explode("\n",$__code,2);$__code_head = $__code_arr[0];$__code_head_arr = explode("//",$__code_head);$sign = array_pop($__code_head_arr);if(md5(substr($__code_arr[1],2))!=$sign){echo "sign error\n";}//48b08bc8b18edd5600f2aaa2b863691e
 ?><?php
 /**
  * User: sdm
